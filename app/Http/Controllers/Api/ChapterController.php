348f6a56a0b8a8fd86e853ca476cb9c0d7caca24
Code with Luis <codewithluis11@gmail.com>
2022-06-22 22:57:03 -0600

    Add Chapter model, migration, controller, api routes

---
+++ app/Http/Controllers/Api/ChapterController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreChapterRequest;
+use App\Http\Requests\UpdateChapterRequest;
+use App\Models\Chapter;
+
+class ChapterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Chapter::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreChapterRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreChapterRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Chapter  $chapter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chapter $chapter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateChapterRequest  $request
+     * @param  \App\Models\Chapter  $chapter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateChapterRequest $request, Chapter $chapter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Chapter  $chapter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chapter $chapter)
+    {
+        //
+    }
+}
