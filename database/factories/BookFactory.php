e8d35d4a3f6e8428139df58ebd05ff257fbbdf3f
Code with Luis <codewithluis11@gmail.com>
2022-06-27 22:29:01 -0600

    Lesson 7: Book model, controller, routes and seeder

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'price' => rand(25, 100),
+            'date_published' => $this->faker->dateTimeBetween('2020-01-01', '2022-01-01')
+        ];
+    }
+}
